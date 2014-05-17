7d499736b5200d6e0500e880507355e7d4f52af7
cygairko <cygairko@gmail.com>
2014-05-17 23:33:54 +0200

    changes

--- nf_mqtt.py
+++ nf_mqtt.py
@@ -74,7 +74,7 @@ def on_message(mosq, obj, msg):
                 color = [int(decoded['color'][0]), int(decoded['color'][1]), int(decoded['color'][2])]
                 led.setColor(color[0], color[1], color[2])
                 send_statusupdate(mosq)
-                print(source + ' set ' + led.getColor())
+                print(source + ' set ' + str(led.getColor()))
             elif (function == 'getcolor'):
                 send_statusupdate(mosq)
             else:
